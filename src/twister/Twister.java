e82b8d1f852cd2790373200d84d93448a36ff504
52645341 <52645341@T140J2401089645>
2024-10-22 19:02:07 -0300

    twister entrega intento 2

---
+++ src/twister/Twister.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package twister;
+
+/**
+ *
+ * @author 52645341
+ */
+public class Twister {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
